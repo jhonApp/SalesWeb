01a5ff6b0f7883175b57e918e9e98b4ab4b9d291
Jhon Everton <48407983+jhonApp@users.noreply.github.com>
2022-03-30 21:48:39 -0300

    Others Class

--- SalesWeb/Models/Department.cs
+++ SalesWeb/Models/Department.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 namespace SalesWeb.Models
 {
@@ -7,5 +10,26 @@ namespace SalesWeb.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int iD, string name)
+        {
+            ID=iD;
+            Name=name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSeles(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
